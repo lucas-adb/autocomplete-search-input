3370a700c14fdcdd9d09b097b0c2222fee20278a
lucas-adb <alvesdebrito.lucas@gmail.com>
2025-08-01 16:29:38 -0300

    feat: fetch cities by query

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AutocompleteSearchInput from './components/AutocompleteSearchInput';
 
 const dataListOptions = [
@@ -7,12 +7,52 @@ const dataListOptions = [
   { value: 'orange', label: 'Orange' },
 ];
 
+interface City {
+  nome: string;
+  id: number;
+  estado: string;
+}
+
 function App() {
   const [query, setQuery] = useState('');
+  const [cities, setCities] = useState<City[]>([]);
+
+  useEffect(() => {
+    if (!query.trim()) {
+      setCities([]);
+      return;
+    }
+
+    async function startFetching() {
+      try {
+        const result = await fetch(
+          `https://brasilapi.com.br/api/cptec/v1/cidade/${query}`
+        );
+
+        if (!result.ok) {
+          console.error('fetch not working');
+          setCities([]);
+          return;
+        }
+
+        const data = await result.json();
+        setCities(data);
+      } catch (error) {
+        console.error('Error fetching cities:', error);
+        setCities([]);
+      }
+    }
+
+    const timeoutId = setTimeout(startFetching, 300);
+
+    return () => clearTimeout(timeoutId);
+  }, [query]);
 
   const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
   };
+
+  console.log('cities', cities);
 
   return (
     <>
